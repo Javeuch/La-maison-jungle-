a29d6323f52afe35021f5d46bf03f7a040e6464e
Javeuch <web@linuxconfig.org>
2022-06-09 15:25:26 +0200

    Section 2 Chap 3 (43%)

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,12 +1,24 @@
 import { plantList } from "../datas/plantList";
 
+const categoryList = plantList.reduce((accumulator, item) =>
+  accumulator.includes(item.category) ? accumulator : accumulator.concat(item.category),
+  []
+)
+
 function ShoppingList() {
   return (
-    <ul>
-      {plantList.map((id) => (
-        <li key={id}>{plantList.name}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {categoryList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {plantList.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
